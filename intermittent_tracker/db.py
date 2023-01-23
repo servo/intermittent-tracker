9fa22a3b71d81e07e1434962031868066e5beca3
Delan Azabani <dazabani@igalia.com>
2023-01-23 23:24:13 +0800

    db: rename and generalise IntermittentDB and intermittents.json

--- intermittent_tracker/db.py
+++ intermittent_tracker/db.py
@@ -1,38 +1,43 @@
 import json
 
-class IntermittentsDB:
+class IssuesDB:
     def __init__(self, db):
-        self.intermittents = db
+        self.data = db
 
 
     def query(self, name):
         # In Python 3 filter() is lazy, so we build an array right here so that it can be jsonified
-        return [x for x in filter(lambda i: name in i['title'], self.intermittents)]
+        return [x for x in filter(lambda i: name in i['title'], self.data)]
 
 
     def add(self, name, number):
-        for i in self.intermittents:
+        for i in self.data:
             if i['number'] == number:
                 return
-        self.intermittents.extend([{'title': name, 'number': number}])
+        self.data.extend([{'title': name, 'number': number}])
 
     
     def remove(self, number):
-        for idx, i in enumerate(self.intermittents):
+        for idx, i in enumerate(self.data):
             if i['number'] == number:
-                self.intermittents.pop(idx)
+                self.data.pop(idx)
                 return
 
 
-class AutoWriteDB(IntermittentsDB):
+class AutoWriteDB:
     def __init__(self, filename):
         self.filename = filename
         with open(filename) as f:
-            IntermittentsDB.__init__(self, json.loads(f.read()))
+            super().__init__(json.loads(f.read()))
 
     def __enter__(self):
         return self
 
     def __exit__(self, etype, evalue, etrace):
         with open(self.filename, 'w') as f:
-            f.write(json.dumps(self.intermittents))
+            f.write(json.dumps(self.data))
+
+
+class AutoWriteIssuesDB(AutoWriteDB, IssuesDB):
+    def __init__(self, filename):
+        super().__init__(filename)
