929854f14d2081731d9a99d7cd70065644ed763a
Delan Azabani <dazabani@igalia.com>
2023-02-02 15:14:10 +0800

    DashboardDB: use generated columns to fix NULL constraint bug

--- intermittent_tracker/db.py
+++ intermittent_tracker/db.py
@@ -59,14 +59,6 @@ class DashboardDB:
             break
 
     @staticmethod
-    def encode_subtest(subtest):
-        return '' if subtest is None else f'={subtest}'
-
-    @staticmethod
-    def decode_subtest(subtest):
-        return subtest[1:] if len(subtest) > 0 else None
-
-    @staticmethod
     def migrate(filename='data/dashboard.sqlite'):
         con = connect(filename)
         version = 0
@@ -98,7 +90,6 @@ class DashboardDB:
 
     def insert_attempt(self, *, path, subtest=None, expected, actual, time=None,
                        message=None, stack=None, branch=None, build_url=None, pull_url=None):
-        subtest = DashboardDB.encode_subtest(subtest)
         if time is None:
             time = now()
         self.con.execute('SAVEPOINT "insert_attempt"')
--- schema/dashboard.1.sql
+++ schema/dashboard.1.sql
@@ -5,39 +5,21 @@ CREATE TABLE "meta" (
 -- dashboard.2.sql should UPDATE "meta" SET "version" = 2; and so on
 INSERT INTO "meta" VALUES (1);
 
--- tests are identified by (path,subtest), but some tests are not subtests.
--- the obvious representation for such tests would be (path,NULL), but sqlite
--- makes the (legal) decision to allow (path,NULL) to effectively bypass
---
--- 1.  PRIMARY KEY constraints (“Each row in a table with a primary key must have a unique
---     combination of values in its primary key columns. For the purposes of determining the
---     uniqueness of primary key values, NULL values are considered distinct from all other
---     values, including other NULLs.”)
---
--- 2.  UNIQUE constraints (“For each UNIQUE constraint on the table, each row must contain a unique
---     combination of values in the columns identified by the UNIQUE constraint. For the purposes of
---     UNIQUE constraints, NULL values are considered distinct from all other values, including
---     other NULLs.”)
---
--- 3.  FOREIGN KEY constraints (“The foreign key constraint is satisfied if for each row in the
---     child table either one or more of the child key columns are NULL, or there exists a row in
---     the parent table for which each parent key column contains a value equal to the value in its
---     associated child key column.”)
---
--- we want (path,NULL) to be treated like an ordinary value, but we can’t have
--- that, so let’s represent tests here as (path,'') and (path,'='+subtest).
-
 CREATE TABLE "test" (
     "path" TEXT NOT NULL                        -- test name
-    , "subtest" TEXT NOT NULL                   -- if subtest, '='+subtest, else ''
+    , "_subtest" TEXT NOT NULL AS (IIF(         -- for constraints [1]
+        "subtest" IS NULL, '', '=' || "subtest"))
+    , "subtest" TEXT                            -- actual subtest name
     , "unexpected_count" INTEGER NOT NULL
     , "last_unexpected" INTEGER                 -- unix time
-    , PRIMARY KEY ("path", "subtest")
+    , UNIQUE ("path", "_subtest")
 );
 
 CREATE TABLE "attempt" (
     "path" TEXT NOT NULL                        -- test name
-    , "subtest" TEXT NOT NULL                   -- if subtest, '='+subtest, else ''
+    , "_subtest" TEXT NOT NULL AS (IIF(         -- for constraints [1]
+        "subtest" IS NULL, '', '=' || "subtest"))
+    , "subtest" TEXT                            -- actual subtest name
     , "expected" TEXT NOT NULL                  -- status e.g. PASS
     , "actual" TEXT NOT NULL                    -- status e.g. FAIL
     , "time" INTEGER NOT NULL                   -- unix time
@@ -46,9 +28,18 @@ CREATE TABLE "attempt" (
     , "branch" TEXT DEFAULT NULL                -- e.g. auto, try
     , "build_url" TEXT DEFAULT NULL             -- e.g. https://github.com/servo/servo/actions/runs/4030556190/jobs/6929482570
     , "pull_url" TEXT DEFAULT NULL              -- e.g. https://github.com/servo/servo/pull/29306
-    , FOREIGN KEY ("path", "subtest") REFERENCES "test"
+    , FOREIGN KEY ("path", "_subtest") REFERENCES "test"
 );
 
 CREATE INDEX "attempt.test" ON "attempt" ("path", "subtest");
 CREATE INDEX "attempt.build" ON "attempt" ("build_url");
 CREATE INDEX "attempt.pull" ON "attempt" ("pull_url");
+
+-- [1] tests are identified by (path,subtest), or (path,NULL) if the test is not
+-- a subtest, but sqlite makes the (legal) decision to let (path,NULL) bypass
+-- PRIMARY KEY constraints, UNIQUE constraints, and FOREIGN KEY constraints.
+-- we can work around this by mapping NULL to '' and 'foo' to '='||'foo' in a
+-- generated column, giving us the behaviour we want when used in constraints.
+-- https://sqlite.org/lang_createtable.html#the_primary_key
+-- https://sqlite.org/lang_createtable.html#unique_constraints
+-- https://sqlite.org/foreignkeys.html#fk_basics
