2ea388a01004437b291eee67566e1f782cb3be8c
Delan Azabani <dazabani@igalia.com>
2023-02-09 18:53:12 +0800

    dashboard: fix a variety of bugs in {GET,POST} /dashboard/attempts

--- intermittent_tracker/dashboard.py
+++ intermittent_tracker/dashboard.py
@@ -1,8 +1,5 @@
 from .db import DashboardDB, IssuesDB
 import json
-import time
-
-FILTER_KEYS = ['path', 'subtest', 'expected', 'actual', 'branch', 'build_url', 'pull_url']
 
 def issues_mixin(path):
     issues = IssuesDB.readonly()
@@ -21,21 +18,10 @@ def tests(request):
 
 def get_attempts(request):
     db = DashboardDB()
-    result = []
-    where = ''
-    params = []
-    for key in FILTER_KEYS:
-        if key in request.args:
-            where += f' AND "{key}" = ?'
-            params.append(request.args[key])
-    if 'since' in request.args:
-        where += ' AND "attempt_id" > ?'
-        params.append(int(request.args['since']))
-    start = time.monotonic_ns()
-    for attempt in db.con.execute(f'SELECT "path", "subtest", "attempt".*, "branch", "build_url", "pull_url" FROM "attempt", "test", "submission" WHERE "test" = "test_id" AND "submission" = "submission_id" AND "actual" != "expected" {where} ORDER BY "attempt_id"', params).fetchall():
-        result.append(dict(attempt))
-    print(f'debug: GET /dashboard/attempts query took {time.monotonic_ns() - start} ns')
-    return json.dumps(result)
+    filters = request.args.to_dict()
+    since = filters.pop('since', None)
+    since = int(since) if since is not None else None
+    return json.dumps(db.select_attempts(since=since, **filters))
 
 def post_attempts(request):
     db = DashboardDB()
